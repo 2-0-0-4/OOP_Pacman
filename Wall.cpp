9cc46e3a3e6e9f5a41e23a64318b4250d19a4039
2-0-0-4 <afeeraumair@gmail.com>
2024-11-22 21:04:08 +0500

    changed color (change it back if its ugly)

--- Wall.cpp
+++ Wall.cpp
@@ -5,16 +5,18 @@
 #include "character.hpp"
 #include "food_poison.hpp"
 
-class Wall{
-    protected:
+class Wall
+{
+protected:
     sf::RectangleShape wall;
-    sf::Vector2f position ;
+    sf::Vector2f position;
 
 public:
-    void draw(sf::RenderWindow &temp, int x_coord, int y_coord){
+    void draw(sf::RenderWindow &temp, int x_coord, int y_coord)
+    {
         sf::RectangleShape wall(sf::Vector2f(16, 16));
-        wall.setFillColor(sf::Color::Blue);
-        wall.setPosition(x_coord*16, y_coord*16);
+        wall.setFillColor(sf::Color(32, 81, 190)); // Light Blue
+        wall.setPosition(x_coord * 16, y_coord * 16);
         temp.draw(wall);
     }
 };
